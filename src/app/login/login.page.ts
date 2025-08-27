97e54c1bd6b35fc1a9037945c7d508e984dda8ae
Luna Marrano <lunamarrano2@gmail.com>
2025-08-27 19:15:12 -0300

    Pantalla login y register

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule, 
+    RouterModule
+  ]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  showPassword = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
-
 }
