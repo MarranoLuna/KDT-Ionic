1b01ad56cf1b56f1c0c49c6ccf31d894293481b4
Luna Marrano <lunamarrano2@gmail.com>
2025-08-27 19:26:48 -0300

    correccion

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule, 
+    RouterModule
+  ]
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  showPassword = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
-
 }
