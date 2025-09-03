30b8ede3301d7785358ed801dc51a2b06a97c407
Federico Guigou <164429673+federicoded@users.noreply.github.com>
2025-09-03 16:47:46 -0300

    cuando inicia ahora lleva al index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
     path: 'index',
     loadComponent: () => import('./index/index.page').then( m => m.IndexPage)
   },
