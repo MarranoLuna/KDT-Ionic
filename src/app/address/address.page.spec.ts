1587da5a639013f010f96ad0ce088da284894746
Ronconi Elisa María <132313233+ElisaRonconi@users.noreply.github.com>
2025-09-18 20:21:09 -0300

    comentar

--- src/app/address/address.page.spec.ts
+++ src/app/address/address.page.spec.ts
@@ -1,17 +1,18 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { AddressPage } from './address.page';
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AddressPage } from './address.page';
 
-// describe('AddressPage', () => {
-//   let component: AddressPage;
-//   let fixture: ComponentFixture<AddressPage>;
+describe('AddressPage', () => {
+  let component: AddressPage;
+  let fixture: ComponentFixture<AddressPage>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AddressPage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddressPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+}); */
+
