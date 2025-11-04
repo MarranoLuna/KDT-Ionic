6b007a59e31b23a09f90df0b899f95bf60af9227
Elisa Ronconi <elisaronconi28@gmail.com>
2025-11-04 02:15:43 -0300

    corrección de conflictos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,58 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule, NavController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { Preferences } from '@capacitor/preferences'; 
 import { UserService } from 'src/app/services/user';
 import { Router } from '@angular/router';
-import { Preferences } from '@capacitor/preferences';
+
+import { 
+  IonMenu,  IonHeader,  IonToolbar,  IonTitle,  IonContent, IonList, 
+  IonItem,  IonIcon, IonLabel,  IonFooter,  IonButton , IonItemDivider
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule]
+ selector: 'app-menu',
+ templateUrl: './menu.component.html',
+ styleUrls: ['./menu.component.scss'],
+ standalone: true,
+ imports: [CommonModule, RouterModule,IonMenu, 
+    IonHeader, IonToolbar,  IonTitle, IonContent, 
+    IonList,  IonItem,  IonIcon, IonLabel,  IonFooter,   IonButton,IonItemDivider,
+ ]
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
-  // Variable pública para guardar el rol ('user' o 'kdt')
-  public userRole: string | null = null;
+ public userRole: string | null = null;
 
-  constructor(private navController: NavController, private userService: UserService, private router: Router,) { }
+ constructor(
+    private navController: NavController, 
+    private userService: UserService, 
+    private router: Router
+  ) { }
 
-  // cargar el rol una vez al inicio
-  async ngOnInit() {
-    await this.loadUserRole();
+ async ngOnInit() {
+  await this.loadUserRole();
+ }
+
+ ionViewWillEnter() {
+  this.loadUserRole(); 
+ }
+
+ async loadUserRole() {
+  const { value } = await Preferences.get({ key: 'userRole' });
+  this.userRole = value ? value.toLowerCase() : null;
+  console.log('Rol cargado en el menú:', this.userRole);
+ }
+
+ async logout() {
+  await Preferences.remove({ key: 'authToken' });
+  await Preferences.remove({ key: 'userRole' });
+  await Preferences.remove({ key: 'user' });
+  this.navController.navigateRoot('/login');
+ }
+
+  async onBecomeCourierClick() {
+    const hasApplied = await this.userService.hasCourierApplication(); 
+    if (hasApplied) {
+      this.router.navigateByUrl('/request-sent-kdt');
+    } else {
+      this.router.navigateByUrl('/kdt-form');
+    }
   }
-
-  //  si el rol pudiera cambiar sin recargar la app
-  ionViewWillEnter() {
-    this.loadUserRole(); 
-
-  constructor(private navController: NavController) {
-    this.loadUserRole();
-  }
-
-  async loadUserRole() {
-    const { value } = await Preferences.get({ key: 'userRole' });
-    this.userRole = value ? value.toLowerCase() : null;
-    console.log('Rol cargado en el menú:', this.userRole);
-  }
-
-  async logout() {
-    await Preferences.remove({ key: 'authToken' });
-    await Preferences.remove({ key: 'userRole' });
-    await Preferences.remove({ key: 'user' });
-    this.navController.navigateRoot('/login');
-  }
-
-async onBecomeCourierClick() {
-
- const hasApplied = await this.userService.hasCourierApplication(); 
- if (hasApplied) {
-   this.router.navigateByUrl('/request-sent-kdt');
-} else {
-this.router.navigateByUrl('/kdt-form');
-}}
 }
